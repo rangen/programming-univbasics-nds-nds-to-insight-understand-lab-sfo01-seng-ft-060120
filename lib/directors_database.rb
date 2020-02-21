de6782f0494c634ad87d4295e803bab2280960ff
Don Mallory <dmm333@gmail.com>
2020-02-21 17:19:27 +0000

    Done.

--- lib/directors_database.rb
+++ lib/directors_database.rb
@@ -4,3 +4,20 @@ def directors_database
 	rot13 = -> (s) { s.tr('A-Za-z', 'N-ZA-Mn-za-m') }
   @_db ||= YAML.load(rot13.call(File.open("directors_db").read.to_s))
 end
+
+def pretty_print_nds(db)
+	pp(db)
+end
+
+def print_first_directors_movie_titles(db)
+	spielberg_movies = db[0][:movies]
+	movieList = []
+	index = 0
+
+	while index < spielberg_movies.length do
+		movieList << spielberg_movies[index][:title]
+		index += 1
+	end
+	puts movieList
+	return movieList.join("\n")
+end
--- lib/nds_explore.rb
+++ lib/nds_explore.rb
@@ -3,9 +3,19 @@ require 'directors_database'
 # Call the method directors_database to retrieve the NDS
 
 def pretty_print_nds(nds)
-  # Change the code below to pretty print the nds with pp
+  pp nds
   nil
 end
 
 def print_first_directors_movie_titles
+  	db = directors_database
+    spielberg_movies = db[0][:movies]
+  	movieList = []
+  	index = 0
+
+  	while index < spielberg_movies.length do
+  		movieList << spielberg_movies[index][:title]
+  		index += 1
+  	end
+  	puts movieList.join("\n")
 end
